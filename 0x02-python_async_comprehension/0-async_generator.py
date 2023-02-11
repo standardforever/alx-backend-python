d51905267e57097825dc7ec530b8ec666e614e2e
standardforver <standard.forver123@gmail.com>
2023-02-11 08:06:08 -0500

    yield value updated

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -10,4 +10,4 @@ async def async_generator() -> Generator[float, None, None]:
     """Using yeild for a random number and wait for 1sec"""
     for _ in range(10):
         await asyncio.sleep(1)
-        yield random.uniform(0, 10)
+        yield random.uniform(1, 10)
