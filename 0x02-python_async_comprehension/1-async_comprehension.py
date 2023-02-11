aeffb4e9591c6e5f14fbecd36afc423ca7c7c97b
standardforver <standard.forver123@gmail.com>
2023-02-11 08:10:05 -0500

    annotated value updated

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -2,8 +2,10 @@
 """Async function"""
 
 import asyncio
+from typing import List
 async_generator = __import__('0-async_generator').async_generator
 
-async def async_comprehension() -> list[float]:
+
+async def async_comprehension() -> List[float]:
     """Call the async_generator and return the output"""
     return [i async for i in async_generator()]
