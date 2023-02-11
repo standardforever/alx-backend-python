f353197fe0572799dd2a9451cce2e7127e2a15b1
standardforver <standard.forver123@gmail.com>
2023-02-11 08:12:28 -0500

    updated

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -5,6 +5,7 @@ import asyncio
 import time
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
+
 async def measure_runtime() -> float:
     """return the time to execute the function async_comprehension 4 time"""
     prev = time.perf_counter()
