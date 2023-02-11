d3797c0de10df469d0d0583ee368f60754c28ff9
standardforver <standard.forver123@gmail.com>
2023-02-11 07:57:32 -0500

    annotaded value updated

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -6,7 +6,7 @@ import random
 from typing import Generator
 
 
-async def async_generator() -> Generator[int, None, None]:
+async def async_generator() -> Generator[float, None, None]:
     """Using yeild for a random number and wait for 1sec"""
     for _ in range(10):
         await asyncio.sleep(1)
